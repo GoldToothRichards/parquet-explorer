3aa09ac736d4844b995c81a4cc614bd6b0c9a40e
GoldToothRichards <crabtr26@proton.me>
2024-10-07 06:07:07 -0400

    fix: Output build to dist folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
 import { withJuno } from "@junobuild/nextjs-plugin";
 
-export default withJuno({ juno: { container: true } });
+export default withJuno({
+  juno: { container: true },
+  nextConfig: {
+    distDir: "dist",
+  },
+});
