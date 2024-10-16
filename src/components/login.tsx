813f3915e0992abb399706ff35f5ef7ab032fa7c
GoldToothRichards <crabtr26@proton.me>
2024-10-15 21:40:29 -0400

    Revert "refactor: Use NFID login"
    This reverts commit e79fc010b6cf8cb47d37df61cdf33914fff83be1.

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,20 +1,6 @@
 import { Button } from "@/components/button";
-import { signIn, NFIDProvider } from "@junobuild/core-peer";
+import { signIn } from "@junobuild/core-peer";
 
 export const Login = () => {
-  const handleSignIn = async () => {
-    try {
-      await signIn({
-        provider: new NFIDProvider({
-          appName: "Parquet Explorer",
-          logoUrl: "https://jacobcrabtree.me/favicon.ico",
-        }),
-      });
-    } catch (error) {
-      console.error("Sign in failed:", error);
-      // Handle error (e.g., show error message to user)
-    }
-  };
-
-  return <Button onClick={handleSignIn}>Sign in with NFID</Button>;
+  return <Button onClick={signIn}>Sign in</Button>;
 };
